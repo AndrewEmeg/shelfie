eeaa99a525267d17f8da7a47554cac18b82d1201
Andrew Emeghebo <andrewemeghebo2023@gmail.com>
2024-07-21 02:45:34 -0400

    still trying

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,11 +1,7 @@
 function Home() {
   return (
     <section className="h-screen text-center my-0 mx-auto max-w-screen-sm">
-      <img
-        className="mx-auto"
-        src="public/img/shelfie-logo.png"
-        alt="Shelfie Logo"
-      />
+      <img className="mx-auto" src="img/shelfie-logo.png" alt="Shelfie Logo" />
       <h1 className="text-7xl text-teal-700 mb-6">Welcome to Shelfie</h1>
       <p className="text-3xl text-slate-400	">
         Discover and review your favorite books
